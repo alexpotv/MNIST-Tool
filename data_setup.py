9a8a58683498a44ebe283c8a5031acc7cfa9f81d
Alexandre Potvin-Demers <alexandre.potvin-demers.1@ulaval.ca>
2020-05-05 00:04:52 -0400

    Added Doxygen compatible documentation

--- data_setup.py
+++ data_setup.py
@@ -1,32 +1,63 @@
+## @package data_setup
+#  @author Alexandre Potvin-Demers
+#  @date 04-05-2020
+#  @brief Contains functions which help format external files in order to match the criteria of the
+#  different datasets.
+#  @details Contains functions which help format external files in order to match the criteria of
+#  the different datasets. It transforms user-made files or external files in a format that can be
+#  interpreted successfully by the trained models of the program, following the constraints of the
+#  datasets used to train the models.
+
 import cv2
 from scipy import ndimage
 import math
 import numpy as np
 
-# Returns the coordinates of the center of mass in order to shift the image
-def getBestShift(img):
-    cy,cx = ndimage.measurements.center_of_mass(img)
-
-    rows,cols = img.shape
-    shiftx = np.round(cols/2.0-cx).astype(int)
-    shifty = np.round(rows/2.0-cy).astype(int)
-
-    return shiftx,shifty
-
-# Shifts the image in parameter according to the coordinates given
-def shift(img,sx,sy):
-    rows,cols = img.shape
-    M = np.float32([[1,0,sx],[0,1,sy]])
-    shifted = cv2.warpAffine(img,M,(cols,rows))
-    return shifted 
-
 # Prepares the file for the model. Loads the specified image file in grayscale, and resizes to 28x28.
+## @function prepare
+#  @brief Prepares a file for its interpretation by a MNIST model.
+#  @details Prepares a file for its interpretation by a MNIST model. The function loads the
+#  specified file as a grayscale image, resizes it to fit the 28x28 criteria, removes the black rows
+#  and columns around the actual pixels representing the drawn number, shifts the image according to
+#  the center of mass and fits it inside a 20x20, centered in the original 28x28 image.
+#  @param file The local path to the number image file to prepare
+#  @returns Returns the modified image as a numpy array, ready for interpretation
 def prepare(file):
+
+    ## @function getBestShift
+    #  @brief Calculates the shift of an image according to its center of mass.
+    #  @details Calculates the shift of an image according to its center of mass, and gets the
+    #  coordinates in which to shift the image to center it according to said center of mass.
+    #  @param img The image to analyse and determinate shift coordinates
+    #  @returns Returns a tuple of the x and y coordinates to shift the image to.
+    def getBestShift(img):
+        cy,cx = ndimage.measurements.center_of_mass(img)
+
+        rows,cols = img.shape
+        shiftx = np.round(cols/2.0-cx).astype(int)
+        shifty = np.round(rows/2.0-cy).astype(int)
+
+        return shiftx,shifty
+
+
+    ## @function shift
+    #  @brief Shifts the image provided according to the coordinates provided.
+    #  @details Shifts the image provided according to the coordinates provided.
+    #  @param img The image to shift
+    #  @param sx The x coordinate to shift the image to
+    #  @param sy The y coordinate to shift the image to
+    #  @returns Returns the shifted image.
+    def shift(img,sx,sy):
+        rows,cols = img.shape
+        M = np.float32([[1,0,sx],[0,1,sy]])
+        shifted = cv2.warpAffine(img,M,(cols,rows))
+        return shifted 
+
+
     IMG_SIZE = 28
     img_array = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
     img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
 
-    # Removing the outer rows and column which are only black
     while np.sum(img_array[0]) == 0:
         img_array = img_array[1:]
     
@@ -41,7 +72,6 @@ def prepare(file):
     
     rows, cols = img_array.shape
 
-    # Resize to a 20x20 format
     if rows > cols:
       factor = 20.0/rows
       rows = 20
@@ -57,7 +87,6 @@ def prepare(file):
     rowsPadding = (int(math.ceil((28-rows)/2.0)),int(math.floor((28-rows)/2.0)))
     img_array = np.lib.pad(img_array,(rowsPadding,colsPadding),'constant')
 
-    # Shift the 20x20 image according to the center of mass
     shiftx,shifty = getBestShift(img_array)
     shifted = shift(img_array,shiftx,shifty)
     img_array = shifted
--- models.py
+++ models.py
@@ -1,71 +1,101 @@
-## 
+## @package models
+#  @author Alexandre Potvin-Demers
+#  @date 04-05-2020
+#  @brief Contains functions which create models based on certain available datasets.
+#  @details Contains functions which create models based on certain available datasets. As for now,
+#  all models created are sequentials models.
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import data_setup
 
+## @function new_model_MNIST
+#  @brief Creates a new model from the available MNIST dataset in TensorFlow.
+#  @details Creates a new model from the available MNIST dataset in TensorFlow. In order to do so,
+#  the dataset is loaded as four different numpy arrays, which are used to train the model, and
+#  then evaluate its metrics.
+#  @return Returns a tuple of length 3 containing, in order, the loss of the model, the accuracy of
+#  the model and the model itself.
 def new_model_MNIST():
-  mnist = tf.keras.datasets.mnist
+    mnist = tf.keras.datasets.mnist
 
-  (x_train, y_train), (x_test, y_test) = mnist.load_data()
-  x_train, x_test = x_train / 255.0, x_test / 255.0
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
 
-  model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10)
-  ])
-
-  predictions = model(x_train[:1]).numpy()
-
-  tf.nn.softmax(predictions).numpy()
-
-  loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-  loss_fn(y_train[:1], predictions).numpy()
-
-  model.compile(optimizer='adam',
-                loss=loss_fn,
-                metrics=['accuracy'])
-
-  model.fit(x_train, y_train, epochs=10)
-
-  metrics = model.evaluate(x_test,  y_test, verbose=2)
-
-  probability_model = tf.keras.Sequential([
-      model,
-      tf.keras.layers.Softmax()
-  ])
-
-  return (metrics[0], metrics[1], probability_model)
-
-
-def new_model_EMNIST():
-  def generator(dataset):
-    for elem in dataset:
-      yield (elem.get('image'), elem.get('label'))
-
-  emnist_train = tfds.load('emnist/digits', split="train")
-  emnist_test = tfds.load('emnist/digits', split="test")
-
-  model = tf.keras.models.Sequential([
-      tf.keras.layers.Flatten(input_shape=(28, 28)),
-      tf.keras.layers.Dense(128, activation='relu'),
-      tf.keras.layers.Dropout(0.2),
-      tf.keras.layers.Dense(10)
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
     ])
 
-  model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    predictions = model(x_train[:1]).numpy()
 
-  model.fit(generator(emnist_train), epochs=1)
+    tf.nn.softmax(predictions).numpy()
 
-  metrics = model.evaluate(generator(emnist_test), verbose=2)
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-  probability_model = tf.keras.Sequential([
-      model,
-      tf.keras.layers.Softmax()
-  ])
+    loss_fn(y_train[:1], predictions).numpy()
 
-  return (metrics[0], metrics[1], probability_model)
+    model.compile(optimizer='adam',
+                  loss=loss_fn,
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=10)
+
+    metrics = model.evaluate(x_test,  y_test, verbose=2)
+
+    probability_model = tf.keras.Sequential([
+        model,
+        tf.keras.layers.Softmax()
+    ])
+
+    return (metrics[0], metrics[1], probability_model)
+
+
+## @function new_model_EMNIST
+#  @brief Creates a new model from the available EMNIST dataset in TensorFlow.
+#  @details Creates a new model from the available MNIST dataset in TensorFlow. In order to do so,
+#  the dataset is loaded as two different tf.data.dataset objects, which are used to train the
+#  model, and then evaluate its metrics.
+#  @return Returns a tuple of length 3 containing, in order, the loss of the model, the accuracy of
+#  the model and the model itself.
+def new_model_EMNIST():
+
+  ## @function generator
+  #  @brief A generator function for iterating through the dataset dictionary objects.
+  #  @details A generator function for iterating through the dataset dictionary objects. Since a
+  #  tf.data.dataset object is not subscriptable, it is passed to the model.fit and model.evaluate
+  #  functions through a generator function. This function gathers the data from the dictionary and
+  #  formats it so that it can be accepted by the model methods.
+  #  @returns Returns a tuple of length 2 containing, in order, the input and output of the data
+  #  point.
+    def generator(dataset):
+        for elem in dataset:
+            yield (elem.get('image'), elem.get('label'))
+
+
+    emnist_train = tfds.load('emnist/digits', split="train")
+    emnist_test = tfds.load('emnist/digits', split="test")
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(generator(emnist_train), epochs=1)
+
+    metrics = model.evaluate(generator(emnist_test), verbose=2)
+
+    probability_model = tf.keras.Sequential([
+        model,
+        tf.keras.layers.Softmax()
+    ])
+
+    return (metrics[0], metrics[1], probability_model)
